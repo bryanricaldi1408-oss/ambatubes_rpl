47573dad530c5620ee9f1e38ae3f0682c7a054aa
bryan's_laptop <bryan>
2025-11-24 15:35:23 +0700

    finish 80 percent

--- mahasiswa/mahasiswaHome.js
+++ mahasiswa/mahasiswaHome.js
@@ -3,3 +3,10 @@ filterBtn.addEventListener('click', () => {
     const filterPanel = document.getElementById('filterPanel');
     filterPanel.classList.toggle('active');
 });
+
+let classCard = document.getElementsByClassName('class-card');
+for (let i = 0; i < classCard.length; i++) {
+    classCard[i].addEventListener('click', () => {
+        window.location.href = 'mTubesDashboard.html';
+    });
+}
